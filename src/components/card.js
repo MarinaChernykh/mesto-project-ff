43b1132a93ce0be2fec0b051f9f42afe6edf2df4
Marina Chernykh <danmarizza@gmail.com>
2024-06-06 00:29:52 +0300

    add sprint 6 features

--- src/components/card.js
+++ src/components/card.js
@@ -1,29 +1,31 @@
 // Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
-// DOM узлы
-const placesList = document.querySelector('.places__list');
-
-// Функция создания карточки
-function createCard(card, deleteCard){
+// Cозданиe карточки
+export function createCard(card, deleteCard, likeCard) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const cardImage = cardElement.querySelector('.card__image');
+  const placeLikeElement = cardElement.querySelector('.card__like-button');
+
   cardImage.src = card.link;
   cardImage.alt = card.name;
   cardElement.querySelector('.card__title').textContent = card.name;
   cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+  placeLikeElement.addEventListener('click', likeCard);
+
   return cardElement;
 }
 
-// Функция удаления карточки
-function deleteCard(evt) {
+// Удалениe карточки
+export function deleteCard(evt) {
   const eventTarget = evt.target;
   const cardElement = eventTarget.closest('.card');
   cardElement.remove();
 }
 
-// Вывести карточки на страницу
-initialCards.forEach((card) => {
-  const cardElement = createCard(card, deleteCard);
-  placesList.append(cardElement);
-})
+// Добавление/ удаление лайков
+export function likeCard(evt) {
+  evt.target.classList.toggle('card__like-button_is-active');
+}
+
+
---
+++ src/components/modal.js
@@ -0,0 +1,32 @@
+// Открытие попапа
+export function openPopup(popup) {
+  popup.addEventListener('click', closePopupByClick);
+  document.addEventListener('keydown', closePopupByEsc);
+  popup.classList.add('popup_is-animated')
+  popup.classList.add('popup_is-opened');
+}
+
+// Закрытие попапа
+export function closePopup(popup) {
+  popup.classList.remove('popup_is-opened');
+}
+
+// Закрытие попапа по клику на крестик или оверлей
+function closePopupByClick(evt) {
+  if (evt.target.classList.contains('popup__close') || evt.target.classList.contains('popup')){
+    closePopup(evt.currentTarget);
+  }
+}
+
+// Закрытие попапа при нажатии Esc
+function closePopupByEsc(evt) {
+  if (evt.key === 'Escape') {
+    closePopup(document.querySelector('.popup_is-opened'));
+    document.removeEventListener('keydown', closePopupByEsc);
+  }
+}
+
+// Добавление плавности появления попапов
+document.querySelectorAll('.popup').forEach((elem) => {
+  elem.classList.add('popup_is-animated');
+});
